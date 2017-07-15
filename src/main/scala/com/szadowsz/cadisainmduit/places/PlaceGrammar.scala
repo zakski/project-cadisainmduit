5c5126a1c8c849f918beb5bce79aae5596236620
zakski <zak.szadowski+git@gmail.com>
2017-07-15 18:28:43 +0100

    improved grammar generation part II

--- src/main/scala/com/szadowsz/cadisainmduit/places/PlaceGrammar.scala
+++ src/main/scala/com/szadowsz/cadisainmduit/places/PlaceGrammar.scala
@@ -28,7 +28,7 @@ object PlaceGrammar {
     PrefixSufffixGrTerm("Great", List("Great", "Greater"), true, 1),
     PrefixSufffixGrTerm("Low", List("Low", "Lower"), true, 1),
     PrefixSufffixGrTerm("Size", List("Big", "Little"), true, 1),
-    PrefixSufffixGrTerm("High", List("High"), true, 1),
+    PrefixSufffixGrTerm("High", List("Upper", "High", "Higher"), true, 1),
     PrefixSufffixGrTerm("Pre_Regal", List("Royal"), true, 1),
     PrefixSufffixGrTerm("Post_Regal", List("Regis"), false, 1),
     PrefixSufffixGrTerm("Pre", List("Mount", "Camp", "Fort", "Port", "Point"), true, 2),
@@ -38,56 +38,49 @@ object PlaceGrammar {
 
   val lvlNames = List("<<PLACENAME>>", "<<BODY>>", "<<CORE>>")
 
-
-  protected val placePH: String = "<<PLACENAME>>"
-  protected val cardinalPH: String = "<<CARDINAL>>"
-  protected val bodyPH: String = "<<BODY>>"
-  protected val corePH: String = "<<CORE>>"
-  protected val animalPH: String = "<<ANIMAL>>"
-  protected val bibPH: String = "<<BIB>>"
-  protected val colourPH: String = "<<COLOUR>>"
-  protected val empirePH: String = "<<EMPIRE>>"
-  protected val eurPH: String = "<<EUR>>"
-  protected val homelandPH: String = "<<HOMELAND>>"
+  protected val ancientPH: String = "<<ANCIENT>>"
   protected val paradisePH: String = "<<PARADISE>>"
-  protected val treesPH: String = "<<PLANT>>"
+  protected val homelandPH1: String = "<<HOMELAND1>>"
+  protected val homelandPH2: String = "<<HOMELAND2>>"
+  protected val indigPH: String = "<<INDIGENOUS>>"
+  protected val animalPH: String = "<<ANIMAL>>"
+  protected val plantsPH: String = "<<PLANT>>"
+  protected val disPH: String = "<<DISCARD>>"
   protected val otherPH: String = "<<OTHER>>"
-  protected val preTypePH: String = "<<PRETYPE>>"
-  protected val postTypePH: String = "<<POSTTYPE>>"
 
   protected val startFilterUDF: UserDefinedFunction = udf[Boolean, String]((name: String) => {
-    !name.startsWith("The ") && !name.startsWith("El ") && !name.startsWith("La ")
+    !name.startsWith("The ") && !name.startsWith("El ") && !name.startsWith("La ") && !name.startsWith("San ") && !name.startsWith("Santa ") &&
+      !name.startsWith("Santo ")
   })
-  //
-  //  protected val coreFilterUDF: UserDefinedFunction = udf[Boolean, String]((name: String) => {
-  //    !cardinalTypes.contains(name) && !prePlTypes.contains(name) && !postPlTypes.contains(name) && !name.split(" ").exists(n => n.length <= 1)
-  //  })
-  //
-  //  protected val coreTypesFilterUDF: UserDefinedFunction = udf[Boolean, String]((name: String) => {
-  //    name.contains(gramPH)
-  //  })
-  //
-  //  protected val coreTypesFilterUDF2: UserDefinedFunction = udf[Boolean, String]((name: String) => {
-  //    !name.contains(gramPH)
-  //  })
-  //
-  //  protected def getPrePlaceTypes: Set[String] = {
-  //    val places = new CsvReader("./archives/dict/places/prePlaceTypes.csv")
-  //    places.readAll().map(s => s.head.trim).toSet
-  //  }
-  //
-    protected def getTerminusTypes: Map[String, String] = {
-     val animal = new CsvReader("./archives/dict/places/animalTypes.csv").readAll().map(s => s.head.trim -> animalPH)
-//      val bib = new CsvReader("./archives/dict/places/biblicalTypes.csv").readAll().map(s => s.head.trim -> bibPH)
-//      val colour = new CsvReader("./archives/dict/places/colourTypes.csv").readAll().map(s => s.head.trim -> colourPH)
-      val empire = new CsvReader("./archives/dict/places/empireTypes.csv").readAll().map(s => s.head.trim -> empirePH)
-//      val eur = new CsvReader("./archives/dict/places/eurTypes.csv").readAll().map(s => s.head.trim -> eurPH)
-      val homeland = new CsvReader("./archives/dict/places/homelandTypes.csv").readAll().map(s => s.head.trim -> homelandPH)
-      val paradise = new CsvReader("./archives/dict/places/paradiseTypes.csv").readAll().map(s => s.head.trim -> paradisePH)
-      val trees = new CsvReader("./archives/dict/places/plantTypes.csv").readAll().map(s => s.head.trim -> treesPH)
-      val list = homeland  ++ animal /* ++ bib ++ colour */ ++ empire ++ paradise ++ trees
-      list.toMap
-    }
+
+  protected def getTerminusTypes: Map[String, String] = {
+    val disGeneric = new CsvReader("./archives/dict/places/toDiscard/generic.csv").readAll().map(s => s.head.trim -> disPH)
+    val disForeign = new CsvReader("./archives/dict/places/toDiscard/foreign.csv").readAll().map(s => s.head.trim -> disPH)
+    val disNumbers = new CsvReader("./archives/dict/places/toDiscard/numbers.csv").readAll().map(s => s.head.trim -> disPH)
+
+    val discard = disGeneric ++ disForeign ++ disNumbers
+
+    val kepAncient = new CsvReader("./archives/dict/places/toKeep/ancient.csv").readAll().map(s => s.head.trim -> ancientPH)
+    val kepParadise = new CsvReader("./archives/dict/places/toKeep/paradise.csv").readAll().map(s => s.head.trim -> paradisePH)
+    val kepHome1 = new CsvReader("./archives/dict/places/toKeep/home1.csv").readAll().map(s => s.head.trim -> homelandPH1)
+    val kepHome2 = new CsvReader("./archives/dict/places/toKeep/home2.csv").readAll().map(s => s.head.trim -> homelandPH2)
+    val kepIndig = new CsvReader("./archives/dict/places/toKeep/indig.csv").readAll().map(s => s.head.trim -> indigPH)
+    val kepAnimal = new CsvReader("./archives/dict/places/toKeep/animals.csv").readAll().map(s => s.head.trim -> animalPH)
+    val kepPlants = new CsvReader("./archives/dict/places/toKeep/plants.csv").readAll().map(s => s.head.trim -> plantsPH)
+    //    val kepColon = new CsvReader("./archives/dict/places/toKeep/colonial.csv").readAll().map(s => s.head.trim -> empirePH)
+    //    val kepColour = new CsvReader("./archives/dict/places/toKeep/colours.csv").readAll().map(s => s.head.trim -> colourPH)
+    //    val kepGeneric = new CsvReader("./archives/dict/places/toKeep/generic.csv").readAll().map(s => s.head.trim -> genericPH)
+    //    val kepHome = new CsvReader("./archives/dict/places/toKeep/home.csv").readAll().map(s => s.head.trim -> homelandPH)
+    //    val kepRev = new CsvReader("./archives/dict/places/toKeep/revolutionary.csv").readAll().map(s => s.head.trim -> revPH)
+    //    val kepRock = new CsvReader("./archives/dict/places/toKeep/rock.csv").readAll().map(s => s.head.trim -> rockPH)
+    //    val kepSaints = new CsvReader("./archives/dict/places/toKeep/saints.csv").readAll().map(s => s.head.trim -> saintPH)
+
+    val keep = kepAncient ++ kepParadise ++ kepHome1 ++ kepHome2 ++ kepIndig ++ kepAnimal ++ kepPlants
+    //kepGeneric ++ kepRev ++ kepRock ++ kepColon ++ kepColour ++ kepForeign ++ kepHome ++ kepParadise ++ kepSaints
+
+    val list = keep ++ discard
+    list.toMap
+  }
 
   protected def getPostPlaceTypes: Set[String] = {
     val places = new CsvReader("./archives/dict/places/postPlaceTypes.csv")
@@ -101,23 +94,23 @@ object PlaceGrammar {
 
   def getRemainder(df: DataFrame): DataFrame = {
     val start = df.filter(startFilterUDF(col("name")))
-    val gr = GrBuilder(gramList, lvlNames, None,"other",Map())
+    val gr = GrBuilder(gramList, lvlNames, None, "other", Map())
     val (h, b) = gr.getGrammar(start)
     b
   }
 
-  def buildGrammar(df: DataFrame, pop : DataFrame): (DataFrame, DataFrame) = {
+  def buildGrammar(df: DataFrame, pop: DataFrame): (DataFrame, DataFrame) = {
     val start = df.filter(startFilterUDF(col("name")))
     val ototal = start.select(sum(col("total"))).collect().head.getLong(0)
 
     val names = pop.select("name").collect().map(_.toSeq.head.toString)
-    val nameFil = udf((n : String, tot : Long) => names.contains(n) || tot > 1)
+    val nameFil = udf((n: String, tot: Long) => names.contains(n) || tot > 1)
     val gr = GrBuilder(gramList, lvlNames, Option(nameFil), "other", endTypes)
     val (h, b) = gr.getGrammar(start)
 
-//    val dif = ototal - b.select(sum(col("total"))).collect().head.getLong(0)
-//    val totalAdj = udf((n : Long) => if (n > 200000) n - dif else n)
+    //    val dif = ototal - b.select(sum(col("total"))).collect().head.getLong(0)
+    //    val totalAdj = udf((n : Long) => if (n > 200000) n - dif else n)
 
-    (h.select(col("parent"),col("name"), col("total")),b)
+    (h.select(col("parent"), col("name"), col("total")), b)
   }
 }
