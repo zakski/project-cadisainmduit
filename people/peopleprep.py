1ffae24451e5c31e25cde996ae728b54e8d2b805
zakski <zak.szadowski+git@gmail.com>
2025-06-10 07:19:18 +0100

    literacy sanitation

--- people/peopleprep.py
+++ people/peopleprep.py
@@ -15,6 +15,8 @@ resultsDirName = os.path.join(rootDirName, '..\\results')
 dirDictionary1901name = os.path.join(rootDirName, '..\\data\\dict\\census\\ireland\\')
 dicLang1901name = os.path.join(dirDictionary1901name, 'ire_lang_1901.csv')
 dicLang1901NoExName = os.path.join(dirDictionary1901name, 'ire_lang_1901_nonExhaust.csv')
+dicLit1901name = os.path.join(dirDictionary1901name, 'ire_literacy_1901.csv')
+dicLit1901NoExName = os.path.join(dirDictionary1901name, 'ire_literacy_1901_nonExhaust.csv')
 
 # 1901 Census File Read
 dir1901name = os.path.join(rootDirName, '..\\data\\data\\census\\ireland\\1901\\')
@@ -59,6 +61,12 @@ print("1901 Census Capitalise")
 df1901['surnameCap'] = df1901.surname.str.upper()
 df1901['nameCap'] = df1901.name.str.upper()
 
+print("1901 Census Gender Standardisation")
+gender = {'M' : 0, 'F' : 1, 'm' : 0, 'f' : 1}
+df1901['gender'] = df1901['gender'].map(gender)
+df1901 = df1901[df1901['gender'].notnull()]
+df1901['gender'] = df1901['gender'].astype('int64')
+
 print("1901 Census Language Standardisation")
 # Use Non Exhaust to convert errors to NaNs
 #dicLang = (pd.read_csv(dicLang1901name,names=['original','languages'],dtype={'original':'string','languages':'string'},index_col='original')
@@ -75,7 +83,70 @@ df1901 = df1901.join(
         mlb.fit_transform(df1901.pop('languagesSanList')),
         index=df1901.index,
         columns=mlb.classes_))
+df1901.rename(columns = {
+    'Arabic': 'lang_arabic', 
+    'Austrian' : 'lang_austrian', 
+    'Broken-English' : 'lang_broke-eng',
+    'Broken-Irish' : 'lang_broke-ire',
+    'Broken-Scotch' : 'lang_broke-sco',
+    'Carney' : 'lang_carney',
+    'Chinese' : 'lang_chinese',
+    'Dutch' : 'lang_dutch',
+    'English' : 'lang_english',
+    'Flemish' : 'lang_flemish',
+    'French' : 'lang_french',
+    'German' : 'lang_german',
+    'Greek' : 'lang_greek',
+    'Gujarati' : 'lang_gujarati',
+    'Hebrew' : 'lang_hebrew',
+    'Hindustani' : 'lang_hindustani',
+    'Irish' : 'lang_irish',
+    'Italian' : 'lang_italian',
+    'Latin' : 'lang_latin',
+    'Manx' : 'lang_manx',
+    'Norwegian' : 'lang_norwegian',
+    'Russian' : 'lang_russian',
+    'Scotch' : 'lang_scotch',
+    'Spanish' : 'lang_spanish',
+    'Swedish' : 'lang_swedish',
+    'Swiss-French' : 'lang_swiss-french',
+    'Telugu' : 'lang_telugu',
+    'Unknown' : 'lang_unknown',
+    'Welsh' : 'lang_welsh',
+    'Yankey' : 'lang_yankey',
+    'Yiddish' : 'lang_yiddish'},
+    inplace=True)
+df1901.drop(['No-Language'], axis=1)
+print("1901 Census Count = " + str(df1901.shape[0]))
 
+print("1901 Census Literacy Standardisation")
+# Use Non Exhaust to convert errors to NaNs
+#dicLang = (pd.read_csv(dicLang1901name,names=['original','languages'],dtype={'original':'string','languages':'string'},index_col='original')
+#          .to_dict())
+dicLit = (pd.read_csv(dicLit1901NoExName,names=['original','mapped'],dtype={'original':'string','mapped':'string'},index_col='original')
+           .to_dict())['mapped']
+df1901['literacy'] = df1901['literacy'].fillna('Unknown')
+df1901['literacySan'] = df1901['literacy'].map(dicLit)
+df1901 = df1901[df1901['literacySan'].notnull()]
+df1901["literacySanList"] = df1901["literacySan"].str.split(" ", expand=False)
+mlb = MultiLabelBinarizer(sparse_output=True)
+df1901 = df1901.join(
+    pd.DataFrame.sparse.from_spmatrix(
+        mlb.fit_transform(df1901.pop('literacySanList')),
+        index=df1901.index,
+        columns=mlb.classes_))
+df1901.rename(columns = {
+    'Read': 'lit_read',
+    'Write' : 'lit_write',
+    'Unknown' : 'lit_unknown'},
+    inplace=True)
+df1901.drop(['Illiterate'], axis=1)
+print("1901 Census Count = " + str(df1901.shape[0]))
+
+print("1901 Census County Standardisation")
+countryRename = {'Londonderry' : 'Derry',  'Queen\'s Co.': 'Laois', 'King\'s Co.' : 'Offaly'}
+df1901['county'] = df1901['county'].map(countryRename).fillna(df1901['county'])
+df1901 = df1901[df1901['county'].notnull()]
 print("1901 Census Count = " + str(df1901.shape[0]))
 
 df1901.info(verbose=True)
